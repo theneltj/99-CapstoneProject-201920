83a9952b0c37c7491d24c25402148797923041ef
zdanavz <zdanaviz@rose-hulman.edu>
2019-02-06 09:43:46 -0500

    Added Testing Code for Calibrate arm

--- src/m3_run_this_on_robot.py
+++ src/m3_run_this_on_robot.py
@@ -17,11 +17,18 @@ def main():
       2. Communicates via MQTT with the GUI code that runs on the LAPTOP.
     """
 
-    run_test_arm()
+    #run_test_arm()
+    run_test_calibration()
 
 def run_test_arm():
     robot=rosebot.RoseBot()
     robot.arm_and_claw.raise_arm()
+
+def run_test_calibration():
+    robot=rosebot.RoseBot()
+    robot.arm_and_claw.calibrate_arm()
+
+
 
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
