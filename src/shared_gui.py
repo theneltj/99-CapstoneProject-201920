065cc2ec152cce255da4c096591e6f36c79a9cd2
jarskijr <jarskijr@rose-hulman.edu>
2019-02-06 08:53:14 -0500

    I added the team memebers names to the shared files.

--- src/shared_gui.py
+++ src/shared_gui.py
@@ -10,7 +10,7 @@
     -- Lower-level code to interact with the EV3 robot library.
 
   Author:  Your professors (for the framework and lower-level code)
-    and PUT_YOUR_NAMES_HERE.
+    and Tyler Thenell, Jacob Jarski, Zack Z.
   Winter term, 2018-2019.
 """
 
--- src/shared_gui_delegate_on_robot.py
+++ src/shared_gui_delegate_on_robot.py
@@ -3,7 +3,7 @@
   This code is the delegate for handling messages from the shared GUI.
 
   Author:  Your professors (for the framework)
-    and PUT_YOUR_NAMES_HERE.
+    and Tyler Thenell, Jacob Jarski, Zack Z.
   Winter term, 2018-2019.
 """
 
