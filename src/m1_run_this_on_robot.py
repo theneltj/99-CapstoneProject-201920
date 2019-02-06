75886bf2bd8f5066fa1c65ac0b12cb9386ad2c60
theneltj <theneltj@rose-hulman.edu>
2019-02-06 08:48:51 -0500

    Put my name in my m1 files

--- src/m1_run_this_on_robot.py
+++ src/m1_run_this_on_robot.py
@@ -1,7 +1,7 @@
 """
   Capstone Project.  Code to run on the EV3 robot (NOT on a laptop).
   Author:  Your professors (for the framework)
-    and Tyler Thenell.
+    and Tyler J Thenell.
   Winter term, 2018-2019.
 """
 
