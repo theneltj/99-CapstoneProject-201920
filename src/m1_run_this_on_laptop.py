1e26ccd9c004609977ab99460c6e3cb19e0968e4
theneltj <theneltj@rose-hulman.edu>
2019-02-06 08:48:09 -0500

    Put my name in my m1 files

--- src/m1_run_this_on_laptop.py
+++ src/m1_run_this_on_laptop.py
@@ -3,7 +3,7 @@
   Displays the Graphical User Interface (GUI) and communicates with the robot.
 
   Authors:  Your professors (for the framework)
-    and Tyler Thenell.
+    and Tyler J Thenell.
   Winter term, 2018-2019.
 """
 
