c2fe36628c2882aac403b6a3d3a8e39fe7d30b78
jarskijr <jarskijr@rose-hulman.edu>
2019-02-07 09:26:45 -0500

    assorted fixes with rosebot, shared_gui_delegate and m2

--- src/shared_gui_delegate_on_robot.py
+++ src/shared_gui_delegate_on_robot.py
@@ -6,4 +6,16 @@
     and Tyler Thenell, Jacob Jarski, Zack Z.
   Winter term, 2018-2019.
 """
+import rosebot
 
+class ResponderToGUIMessages(object):
+    def __init__(self, robot):
+        """
+
+            :type robot: rosebot.RoseBot
+        """
+        self.robot = robot
+    def go(self, left_wheel_speed, right_wheel_speed):
+        left = int(left_wheel_speed)
+        right = int(right_wheel_speed)
+        self.robot.drive_system.go(left, right)
