c88f34e7c3436307b14b8e42eddc60846178009c
zdanavz <zdanaviz@rose-hulman.edu>
2019-02-07 09:30:57 -0500

    Changing tests around to run different ones

--- src/m3_run_this_on_robot.py
+++ src/m3_run_this_on_robot.py
@@ -18,12 +18,12 @@ def main():
     """
 
     #run_test_arm()
-    run_test_calibration()
+    #run_test_calibration()
 
-    #run_test_move_arm(3000)
-    #run_test_move_arm(5112)
-    #run_test_move_arm(2000)
-    #run_test_move_arm(0)
+    run_test_move_arm(3000)
+    run_test_move_arm(5112)
+    run_test_move_arm(2000)
+    run_test_move_arm(0)
     #real_thing()
 
 
