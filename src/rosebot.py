f85b895c930c617615d3f1b6b99c9566eef68ce2
jarskijr <jarskijr@rose-hulman.edu>
2019-02-07 09:01:12 -0500

    created move_arm_position function

--- src/rosebot.py
+++ src/rosebot.py
@@ -232,15 +232,14 @@ class ArmAndClaw(object):
                 self.motor.turn_on(100)
                 if desired_arm_position <= self.motor.get_position():
                     self.motor.turn_off()
-                    self.motor.reset_position()
                     break
         if desired_arm_position < self.motor.get_position():
             while True:
                 self.motor.turn_on(-100)
                 if desired_arm_position >= self.motor.get_position():
                     self.motor.turn_off()
-                    self.motor.reset_position()
                     break
+
 
     def lower_arm(self):
 
