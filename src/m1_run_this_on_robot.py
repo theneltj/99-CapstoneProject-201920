be6461b57425a93904108a6c04bf6f4f87d4fc40
theneltj <theneltj@rose-hulman.edu>
2019-02-11 12:37:03 -0500

    Testing and debugging

--- src/m1_run_this_on_robot.py
+++ src/m1_run_this_on_robot.py
@@ -18,6 +18,9 @@ def main():
       2. Communicates via MQTT with the GUI code that runs on the LAPTOP.
     """
     robot = rosebot.RoseBot()
+    robot.arm_and_claw.raise_arm()
+    robot.arm_and_claw.lower_arm()
+    robot.sound_system.beeper.beep()
     real_thing()
 
 def real_thing():
