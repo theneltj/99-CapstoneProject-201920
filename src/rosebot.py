6ccffca5966f23f7e6b28c661d59f84d232afcfd
jarskijr <jarskijr@rose-hulman.edu>
2019-02-13 08:38:42 -0500

    get distance in inches

--- src/rosebot.py
+++ src/rosebot.py
@@ -599,6 +599,8 @@ class InfraredProximitySensor(object):
         """
         cm_per_inch = 2.54
         distance = 48 / cm_per_inch * self.get_distance() / 100
+        print(distance)
+        return distance
 
 
 ###############################################################################
