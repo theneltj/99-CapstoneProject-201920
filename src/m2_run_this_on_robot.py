6aaec328a0abb32dd0c9c7993225cf5bbd2f187c
jarskijr <jarskijr@rose-hulman.edu>
2019-02-13 08:53:36 -0500

    get distance in inches

--- src/m2_run_this_on_robot.py
+++ src/m2_run_this_on_robot.py
@@ -18,7 +18,7 @@ def main():
     # robot.arm_and_claw.move_arm_to_position(180*14.2)
     # robot.arm_and_claw.lower_arm()
     robot.sound_system.beeper.beep()
-    robot.drive_system.go_backward_until_distance_is_greater_than(10, 20)
+    robot.drive_system.go_backward_until_distance_is_greater_than(5, 20)
     robot.sound_system.beeper.beep()
 
 
