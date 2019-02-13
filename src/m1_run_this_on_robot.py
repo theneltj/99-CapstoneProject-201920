a7af6d44a7beddd8b20a553061ea8ba1ca420cfd
theneltj <theneltj@rose-hulman.edu>
2019-02-13 12:44:04 -0500

    Fixed intensity function while loops

--- src/m1_run_this_on_robot.py
+++ src/m1_run_this_on_robot.py
@@ -19,7 +19,8 @@ def main():
     """
     robot = rosebot.RoseBot()
     robot.sound_system.beeper.beep()
-    real_thing()
+    robot.drive_system.go_straight_until_intensity_is_greater_than(60, 50)
+    # real_thing()
 
 def real_thing():
     robot = rosebot.RoseBot()
