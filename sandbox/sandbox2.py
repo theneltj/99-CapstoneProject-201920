aa8c7b1495eb815906b924b9f3b48aa0bd700d81
jarskijr <jarskijr@rose-hulman.edu>
2019-02-20 22:04:53 -0500

    Sandbox stuff

--- sandbox/sandbox2.py
+++ sandbox/sandbox2.py
@@ -2,7 +2,7 @@
 # It exists here as a place where you can "try out" things without harm.
 def main():
  place = 'Campground'
- action_array = [0, 0, 0, 0, 0]
+ action_array = [0, 0, 0, 0, 0, 0, 0]
 
  def place_discriptions(place, action_array):
      Campground_Description = 'You arrive back at your small campground, the fire still burning brightly.'
@@ -14,8 +14,8 @@ def main():
      Ridge_Description = 'Struck into the rock is a large sledgehammer. You can remove it if you would like'
      Tree_With_Rope_Description = 'In front of you stands an incredibly large oak tree, with rope fastened to climb upon it.' \
                                   'Perhaps the rope could be useful if you cut it?'
-     Shack_Description = 'There appears to be a shack, small but apparently quite sturdy.' \
-                         'The door on the front looks like it can be broken with enough force'
+     Shack_Description = 'You arrive at the shack, but the door is locked. You can see something inside however.' \
+                         'The door could be broken with a large item'
      Plains_Description = 'You arrive in a large plains, surrounded by a large amount of low standing grasses.' \
                           'Galloping in the grass reminds you pleasantly of your childhood.' \
                           'It does not appear like anything other than wasting time can be done here'
@@ -24,6 +24,26 @@ def main():
                          'You take the note with you and place it in your pocket'
      if action_array[0] == 1:
         Gashed_Tree_Description = 'You arrive at a tree that once had a sharp knife in its bark, pulled out by yourself'
+     if action_array[1] == 1:
+         Tree_With_Rope_Description = 'In front of you stands an incredibly large oak tree, with remnants of rope attached'
+     if action_array[4] == 1:
+         Cliff_Description = 'There is a large cliff in front of you, able to be scaled with the rope you have attached.'
+     if action_array[2] == 1:
+         Ridge_Description = 'A large rock has several hammer marks from the large sledgehammer you have taken.'
+     if action_array[5] == 1:
+         Shack_Description = 'You arrive at the shack, but the door is now non-existent due to you bludgeoning it with' \
+                             'a large sledgehammer, a key appears to be inside!'
+     if action_array[3] == 1:
+         Shack_Description = 'You arrive at a shack, but the door is now non-existent due to you bludgeoning it with' \
+                             'a large sledgehammer, only junk and dust appear to reside here'
+     if action_array[6] == 1:
+         House_Description = 'You arrive at the doorstep of the house, but the front door is locked with a key.' \
+                             'A note appears to have been taken. Oh right! You pull the note out of your pocket and' \
+                             'read it again. "Left the key in the shack". You place the note back in your pocket'
+     if action_array[3] == 1:
+         House_Description = 'You arrive at the doorstep of the house, but the front door is locked with a key.' \
+                             'The key you have looks like it would fit!'
+
      if place == 'Campground':
          return Campground_Description
      if place == "Forest":
@@ -61,11 +81,29 @@ def main():
     Plains_Description = 'you see a large amount of plains, almost flat. ' \
                          'There appears to be something in the distance'
     Gashed_Tree_Description = 'you see a glint appearing from a tree, an item of some sort?'
-    Ridge_Description = 'there is a large cliff that is not possible to scale without help. ' \
+    Ridge_Description = 'there is a large ridge that is not possible to scale without help. ' \
                         'There appears to be a large sledgehammer on top of the ridge'
     Shack_Description = 'there appears to be a shack, small but apparently quite sturdy.' \
                         'The door on the front looks like it could be broken with enough force.'
     House_Description = 'There appears to be a large house, resembling the bandits house you have heard of!'
+
+    if action_array[0] == 1:
+        Gashed_Tree_Description = 'The apparent glint from the tree is no longer there now that you have removed the knife'
+
+    if action_array[1] == 1:
+        Tree_With_Rope_Description = 'you see a large oak tree, much larger than the surrounding trees. Nothing' \
+                                     'looks to be attached to it...'
+    if action_array[4] == 1:
+        Ridge_Description = 'there is a large ridge in front of you, able to be scaled with the rope you have attached.' \
+                            'There appears to be a large sledgehammer on top of the cliff.'
+    if action_array[2] == 1:
+        Ridge_Description = 'there is a large ridge in front of you, able to be scaled with the rope you have attached.' \
+                             'The sledgehammer previously seen has now been take by yourself.'
+    if action_array[5] == 1:
+        Shack_Description = 'There appears to be a shack, small but apparently quite sturdy. The door on the front has' \
+                            'been demolished by a large sledgehammer'
+
+
 
     if place == 'Campground':
         North = Plains_Description
@@ -213,13 +251,13 @@ def main():
 
  def direction(place):
      while True:
-         direction = str(input('What direction would you like to go?'))
+         direction = str(input('What direction would you like to go? If you would like to interact instead input "Interact"'))
 
-         if direction == "West" or direction == 'East' or direction == 'South' or direction == 'North':
+         if direction == "West" or direction == 'East' or direction == 'South' or direction == 'North' or direction == 'Interact':
              break
          else:
              print('That is not a correct input, please enter a direction of '
-                   'North, East, South, or West')
+                   'North, East, South, or West. If you want to interact enter "Interact"')
      if place == 'Campground':
          print('You have headed,', direction)
 
@@ -232,6 +270,16 @@ def main():
              print('You have headed,', direction)
 
      if place == 'Cliff':
+         if direction == 'Interact':
+             if action_array[4] == 0:
+                 if action_array[1] == 1:
+                     print('You attach the rope to the latch, allowing you to go up the cliff if you desire')
+                     action_array[4] = 1
+                 else:
+                     print('You do not have anything to attach to the latch!')
+             else:
+                 print('The rope is already attached, there is nothing else to interact with here')
+
          if direction == 'East':
              print('You have headed,', direction)
 
@@ -240,10 +288,25 @@ def main():
                  print('You have climbed up the rope',direction)
 
      if place == 'Ridge':
+         if direction == 'Interact':
+             if action_array[2] == 0:
+                print('You manage to grab the large sledgehammer out quite easily.')
+                action_array[2] = 1
+             else:
+                 print('There is nothing to interact with here.')
          if direction == 'East':
              print('You have climbed down the rope', direction)
 
      if place == 'Tree With Rope':
+         if direction == 'Interact':
+             if action_array[1] == 0:
+                 if action_array[0] == 1:
+                    print('You manage to detach most of the rope after cutting it with your knife')
+                    action_array[1] = 1
+                 if action_array[0] == 0:
+                     print('You do not have anything to cut the rope with!')
+             elif action_array[1] == 1:
+                 print('You have already cut the rope, there is nothing else to interact with here.')
          if direction == 'North' or 'South':
              print('You have headed,', direction)
 
